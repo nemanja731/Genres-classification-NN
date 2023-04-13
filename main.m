8e3abe5be01563cbb095b8897159f1766b24922c
nemanja731 <nemanja.jankovic731@gmail.com>
2023-04-13 05:38:29 +0200

    Update main file

--- main.m
+++ main.m
@@ -2,46 +2,46 @@ clear
 close all
 clc
 
-%% Ucitavanje podataka
+%% Data loading
 T = readtable('genres.csv');
-izlaz = T{:, 12}';
-ulaz = T{:, 1:11};
-N = length(izlaz);
+out = T{:, 12}';
+in = T{:, 1:11};
+N = length(out);
 
-%% Podela na klase
-Rap = strcmp(izlaz', 'Rap')';
-Pop = strcmp(izlaz', 'Pop')';
-RnB = strcmp(izlaz', 'RnB')';
-K1 = ulaz(Rap == 1, :);
-K2 = ulaz(Pop == 1, :);
-K3 = ulaz(RnB == 1, :);
-izlazOH = [Rap;Pop;RnB];
+%% Division into classes
+Rap = strcmp(out', 'Rap')';
+Pop = strcmp(out', 'Pop')';
+RnB = strcmp(out', 'RnB')';
+K1 = in(Rap == 1, :);
+K2 = in(Pop == 1, :);
+K3 = in(RnB == 1, :);
+outOH = [Rap;Pop;RnB];
 
-%% Prikaz klasa
-kategorije = categorical(izlaz);
+%% Class view
+category = categorical(out);
 figure
-    histogram(kategorije, 'BarWidth', 0.7)
-    title('Histogram klasa')
-    xlabel('Klase')
-    ylabel('Broj pojavljivanja klasa')
+    histogram(category, 'BarWidth', 0.7)
+    title('Class histogram')
+    xlabel('Classes')
+    ylabel('Number of class occurrences')
     grid on;
 
-%% Podela podataka na odgovarajuce skupove po klasama
-%podela za klasu 1
+%% Division of data into appropriate sets by classes
+%first class
 N1 = length(K1);
 rng(50);
 K1perm = K1(randperm(N1), :);
 K1trening = K1perm(1 : round(0.7*N1), :);
 K1test = K1perm(round(0.7*N1) + 1 : round(0.85*N1), :);
 K1val = K1perm(round(0.85*N1) + 1 : N1, :);
-%podela za klasu 2
+%second class
 N2 = length(K2);
 rng(50);
 K2perm = K2(randperm(N2), :);
 K2trening = K2perm(1 : round(0.7*N2), :);
 K2test = K2perm(round(0.7*N2) + 1 : round(0.85*N2), :);
 K2val = K2perm(round(0.85*N2) + 1 : N2, :);
-%podela za klasu 3
+%third class
 N3 = length(K3);
 rng(50);
 K3perm = K3(randperm(N3), :);
@@ -49,45 +49,45 @@ K3trening = K3perm(1 : round(0.7*N3), :);
 K3test = K3perm(round(0.7*N3) + 1 : round(0.85*N3), :);
 K3val = K3perm(round(0.85*N3) + 1 : N3, :);
 
-%% Grupisanje u zajednocki trening, test i validacioni skup
-%trening skup
-ulazTrening = [K1trening;K2trening;K3trening];
+%% Grouping into joint training, test and validation sets
+%training
+inTrening = [K1trening;K2trening;K3trening];
 v1 = [ones(1, round(0.7*N1));zeros(2, round(0.7*N1))];
 v2 = [zeros(1, round(0.7*N2));ones(1, round(0.7*N2));zeros(1, round(0.7*N2))];
 v3 = [zeros(2, round(0.7*N3));ones(1, round(0.7*N3))];
-izlazTrening = [v1, v2, v3];
-%test skup
-ulazTest = [K1test;K2test;K3test];
+outTrening = [v1, v2, v3];
+%test
+inTest = [K1test;K2test;K3test];
 v1 = [ones(1, round(0.15*N1));zeros(2, round(0.15*N1))];
 v2 = [zeros(1, round(0.15*N2));ones(1, round(0.15*N2));zeros(1, round(0.15*N2))];
 v3 = [zeros(2, round(0.15*N3));ones(1, round(0.15*N3))];
-izlazTest = [v1, v2, v3];
-%validacioni skup
-ulazVal = [K1val;K2val;K3val];
-izlazVal = izlazTest;
+outTest = [v1, v2, v3];
+%validation
+inVal = [K1val;K2val;K3val];
+outVal = outTest;
 
-%% Zajednicki skup prosledjen neuralnoj mrezi
-ulazSve = [ulazTrening; ulazVal];
-izlazSve = [izlazTrening, izlazVal];
+%% Common set passed to neural network
+inAll = [inTrening; inVal];
+outAll = [outTrening, outVal];
 
-%% Permutovanje elemenata u skupovima
+%% Permutation of elements in sets
 rng(50);
-ind = randperm(length(ulazSve));
-ulazSve = ulazSve(ind, :);
-izlazSve = izlazSve(:, ind);
+ind = randperm(length(inAll));
+inAll = inAll(ind, :);
+outAll = outAll(:, ind);
 rng(50);
-ind = randperm(length(ulazVal));
-ulazVal = ulazVal(ind, :);
-izlazVal = izlazVal(:, ind);
+ind = randperm(length(inVal));
+inVal = inVal(ind, :);
+outVal = outVal(:, ind);
 rng(50);
-ind = randperm(length(ulazTest));
-ulazTest = ulazTest(ind, :);
-izlazTest = izlazTest(:, ind);
+ind = randperm(length(inTest));
+inTest = inTest(ind, :);
+outTest = outTest(:, ind);
 
-%% Krosvalidacija
-% arhitektura = {[20 15 10], [5 10 15], [6 7 8]};
-% arhitektura = {[5 10 15], [10 10 10], [10 10 5]};
-arhitektura = {[20 25 20], [8 9 21], [7 5 12]};
+%% Cross validation
+% architecture = {[20 15 10], [5 10 15], [6 7 8]};
+% architecture = {[5 10 15], [10 10 10], [10 10 5]};
+architecture = {[20 25 20], [8 9 21], [7 5 12]};
 transferFcns = ['poslin'; 'tansig'; 'logsig';];
 numTransFcns = size(transferFcns);
 numTransFcns = numTransFcns(1);
@@ -104,20 +104,20 @@ R2best = 0;
 R3best = 0;
 for reg = [0.05, 0.1, 0.15, 0.2, 0.7, 0.9]
     for t = 1:numTransFcns
-        for arh = 1:length(arhitektura)
+        for arh = 1:length(architecture)
             for w = [0.5, 1, 2, 3]
                 for lr = [0.1, 0.5, 0.8]
                     rng(50);
-                    %kreiranje mreze
-                    net = patternnet(arhitektura{arh});
+                    %creating a network
+                    net = patternnet(architecture{arh});
 
                     net.divideFcn = 'divideind';
-                    net.divideParam.trainInd = 1 : length(ulazTrening);
-                    net.divideParam.valInd = length(ulazTrening) + 1 : length(ulazSve);
+                    net.divideParam.trainInd = 1 : length(inTrening);
+                    net.divideParam.valInd = length(inTrening) + 1 : length(inAll);
                     net.divideParam.testInd = [];
 
                     tf = transferFcns(t, :);
-                    for k = 1:length(arhitektura{arh})
+                    for k = 1:length(architecture{arh})
                         net.layers{k}.transferFcn = tf;
                     end
                     net.layers{k + 1}.transferFcn = 'softmax';
@@ -138,17 +138,17 @@ for reg = [0.05, 0.1, 0.15, 0.2, 0.7, 0.9]
                     %net.trainFcn = 'traingdm';
                     net.trainFcn = 'trainscg';
                     
-                    %druga klasa je mala i zelimo da joj posvetimo vise paznje
-                    weight = ones(1, length(izlazSve));
-                    weight(izlazSve(2,:) == 1) = w;
+                    %second class is small and we want to pay more attention to it
+                    weight = ones(1, length(outAll));
+                    weight(outAll(2,:) == 1) = w;
 
                     %treniranje mreze
-                    net = train(net, ulazSve', izlazSve, [], [], weight);
+                    net = train(net, inAll', outAll, [], [], weight);
 
-                    %predikcija mreze
-                    pred = sim(net, ulazVal');
+                    %prediction
+                    pred = sim(net, inVal');
 
-                    [~, cm] = confusion(izlazVal, pred);
+                    [~, cm] = confusion(outVal, pred);
                     P1 = cm(1,1)/(cm(1,1)+cm(1,2)+cm(1,3));
                     R1 = cm(1,1)/(cm(1,1)+cm(2,1)+cm(3,1));
                     P2 = cm(2,2)/(cm(2,1)+cm(2,2)+cm(2,3));
@@ -160,8 +160,8 @@ for reg = [0.05, 0.1, 0.15, 0.2, 0.7, 0.9]
                     F13 = 2*P3*R3/(P3+R3);
                     F1 = 1/3*(F11+F12+F13);
                     
-                    %provera da li je najbolja mreza trenutna
-                    %kriterijumska funkcija je Fscore zbog nebalansiranih klasa
+                    %checking if the best network is current
+                    %the criterion function is Fscore due to unbalanced classes
                     if F1 > F1best
                         F1best = F1;
                         Bestreg = reg;
@@ -184,13 +184,13 @@ for reg = [0.05, 0.1, 0.15, 0.2, 0.7, 0.9]
    end
 end
 
-%% Projektovanje najbolje mreze
+%% Designing the best network
 rng(50);
-net = patternnet(arhitektura{BestArh});
+net = patternnet(architecture{BestArh});
 
 net.divideFcn = '';
 
-for k = 1:length(arhitektura{arh})
+for k = 1:length(architecture{arh})
     net.layers{k}.transferFcn = BestTransferFunction;
 end
 net.layers{k + 1}.transferFcn = 'softmax';
@@ -203,32 +203,32 @@ net.trainParam.min_grad = 1e-6;
 net.trainParam.max_fail = 100;
 net.trainFcn = 'trainscg';
 
-weight = ones(1, length(izlazSve));
-weight(izlazSve(2,:) == 1) = best_w;
+weight = ones(1, length(outAll));
+weight(outAll(2,:) == 1) = best_w;
 
-%treniranje mreze
-net = train(net, ulazSve', izlazSve, [], [], weight);
+%network training
+net = train(net, inAll', outAll, [], [], weight);
 
-%predikcija mreze
-pred = sim(net, ulazTest');
+%network prediction
+pred = sim(net, inTest');
 pred = round(pred);
-figure, plotconfusion(izlazTest, pred);
-disp('---------KRAJ---------')
+figure, plotconfusion(outTest, pred);
+disp('---------The end---------')
 
-%% Prikaz najboljih vrednosti
-disp('Najbolja arhitektura:')
-disp(arhitektura{BestArh})
-disp('Najbolja konstanta obucavanja:')
+%% Display of the best parameters
+disp('Best architecture:')
+disp(architecture{BestArh})
+disp('Best training constant:')
 disp(best_lr)
-disp('Najbolja konstanta regularizacije:')
+disp('Best regularization constant:')
 disp(Bestreg)
-disp('Najbolja tezina:')
+disp('Best weight:')
 disp(best_w)
-disp('Najbolji broj epoha:')
+disp('Best number of epochs:')
 disp(net.trainParam.epochs)
-disp('Najbolji goal:')
+disp('Best goal:')
 disp(net.trainParam.goal)
-disp('Najbolja minimalna promena gradijenta:')
+disp('Best minimum gradient change:')
 disp(net.trainParam.min_grad)
-disp('Najbolja dozvoljena maksimalna greska:')
+disp('Best the best allowed maximum error:')
 disp(net.trainParam.max_fail)
